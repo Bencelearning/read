330eb9fcf9dda49156bc954b46d5d7ec16536309
Bence <pscheng1203@gmail.com>
2018-10-25 21:58:57 +0800

    add filter in function

--- read.py
+++ read.py
@@ -3,11 +3,26 @@ count = 0
 with open('reviews.txt', 'r') as f: #以讀取模式開啟檔案 reviews.txt 並將開啟的檔案於程式碼中命名為f
 	for line in f:
 		data.append(line)
-		#count += 1
-		#if count % 1000 == 0:
-			#print(len(data)) #透過 count 了解資料寫入 data 的進度
+		count += 1
+		if count % 1000 == 0:
+			print(len(data)) #透過 count 了解資料寫入 data 的進度
 print('讀取完成, 共計', len(data), '筆資料')
+
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-print(sum_len / len(data))
+print('平均字元長度為', sum_len / len(data)) #計算所有留言的平均字元長度
+
+hundred = []
+for d in data:
+	if len(d) < 100:
+		hundred.append(d)
+print('一共有', len(hundred), '筆資料字元少於100') #篩選字元少於100的留言筆數
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆資料提及good') #篩選留言提到 good 的筆數
+
+
